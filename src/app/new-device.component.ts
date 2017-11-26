110be30875c94ac91e904a76059151c3aedc90c6
sowmya dinavahi <sowmya.dsl@gmail.com>
2017-11-26 12:04:44 -0800

    Add newDevice details

--- src/app/new-device.component.ts
+++ src/app/new-device.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Device } from './device.model';
+
 
 @Component({
   selector: 'app-new-device',
   templateUrl: './new-device.component.html',
   styleUrls: ['./new-device.component.css']
 })
-export class NewDeviceComponent implements OnInit {
+export class NewDeviceComponent  {
+  @Output() newDeviceSender = new EventEmitter;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  newDevice (
+   name: string,
+   image: string,
+   location: any,
+   role: string,
+   status: string,
+   history: string,
+   date: string
+ ){
+   var newDevice = new Device(name, image, location, role, status, history, date);
+   this.newDeviceSender.emit(newDevice);
+ }
 
 }
