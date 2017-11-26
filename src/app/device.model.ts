c434ffedb8beef037d5ad6b89ab39750d738217a
sowmya dinavahi <sowmya.dsl@gmail.com>
2017-11-25 16:31:50 -0800

    Add dependencies

---
+++ src/app/device.model.ts
@@ -0,0 +1,10 @@
+export class Device{
+  constructor (
+    public name: string,
+    public image: string,
+    public location: any,
+    public role: string,
+    public status: string,
+    public history: string,
+    public date: any){}
+}
